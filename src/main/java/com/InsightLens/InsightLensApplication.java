cd469ee100df5f0bd4e76a5ca3cb8f7d33ffbd32
manohar <manohar.hello@gmail.com>
2025-04-28 18:41:03 +0530

    Initial commit: Spring Boot + DGS + Spring AI setup

---
+++ src/main/java/com/InsightLens/InsightLensApplication.java
@@ -0,0 +1,13 @@
+package com.InsightLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InsightLens/InsightLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsightLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
